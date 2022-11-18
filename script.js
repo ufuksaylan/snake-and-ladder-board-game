e5870f7eb88165e77193bdd8abf9e5c70e85aa8c
ufuksaylan <ufuksaylannn@gmail.com>
2022-11-18 05:42:19 +0200

    movements of players

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let images = [
+  "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png"
+];
 
 let gameData = undefined;
 
@@ -43,6 +46,8 @@ function addPlayers(){
   for (let i = 0; i < gameData.players.length; i++) {
 
     let playerR = document.createElement( 'h5' );
+    playerR.id = `player${gameData.players[i][0]}`;
+    
     playerR.textContent = `${gameData.players[i][0]}`;
 
     console.log(playerR);
@@ -85,10 +90,87 @@ function addLadders(){
   }
 }
 
+
 function rollDice() {
-  return 1 + Math.floor(Math.random() * 6);
+  if(gameData == undefined){
+    alert("start a game first")
+    return; 
+  }
+
+  let x = 1 + Math.floor(Math.random() * 6);
+
+  document.querySelector("#diceimg").setAttribute("src", images[x-1]);
+  console.log(x);
+
+  movePlayers(x);
+
+  return x;
+}
+function removeElement(id) {
+  var elem = document.getElementById(id);
+  return elem.parentNode.removeChild(elem);
 }
 
+function movePlayers(x){
+  
+  for (let i = 0; i < gameData.players.length; i++) {
+    if (gameData.players[i][2]) {
+      removeElement(`player${gameData.players[i][0]}`);
+      gameData.players[i][2] = false; 
+
+      gameData.players[i][1] += x; 
+      if (gameData.players[i][1] == 30) {
+        alert(`player ${gameData.players[i][0]} won the game. refresh to play again!`);
+
+        removeElement('board');
+        gameData = undefined;
+        return; 
+      }
+
+
+      gameData.players[i][1] = arrangePosition(gameData.players[i][1]);
+
+      let playerR = document.createElement( 'h5' );
+      playerR.id = `player${gameData.players[i][0]}`;
+      
+      playerR.textContent = `${gameData.players[i][0]}`;
+
+      let temp = document.querySelector(`#number${gameData.players[i][1]}`);
+      temp.appendChild(playerR);
+
+
+
+
+
+      if (i+1 >= gameData.players.length)
+        gameData.players[0][2] = true;
+      else
+        gameData.players[i+1][2] = true;    
+
+      return; 
+    }
+  }
+}
+function arrangePosition(player) {
+  if (player > gameData.nfields.length)
+  {
+    x = player - gameData.nfields.length;
+    player = gameData.nfields.length - x; 
+  }
+
+  for (let i = 0; i < gameData.snakes.length; i++) {
+    if (player == gameData.snakes[i][0]) {
+      player = gameData.snakes[i][1];
+    }
+  }
+  for (let i = 0; i < gameData.ladders.length; i++) {
+    if (player == gameData.ladders[i][0]) {
+      player = gameData.ladders[i][1];
+    }
+  }
+
+  return player; 
+}
 function getJson() {
   fetch('./data.json').then(response => {
     return response.json();
@@ -103,4 +185,7 @@ function getJson() {
 
 
 document.querySelector("#json").addEventListener("click", getJson, {once:true});
+
+
+document.querySelector("#roll").addEventListener("click", rollDice);
 
